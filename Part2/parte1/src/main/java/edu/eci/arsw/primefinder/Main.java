ad2343a0421c59b716edb2f6754f2c9a84e9b0c8
unknown <eduardo.ocampo@labinfo.is.escuelaing.edu.co>
2020-01-28 14:28:20 -0500

    2.1.3

--- Part2/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ Part2/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000,1);
+		System.out.println(pft.getPrimes().size());
 		//System.out.println(pft.getPrimes().size());
 
 
--- Part2/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ Part2/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,98 +1,105 @@
 package edu.eci.arsw.primefinder;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-public class PrimeFinderThread extends Thread{
-
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-		this.a = a;
-		this.b = b;
-
-	}
-
-	public PrimeFinderThread(int a, int b,int n) {
-		super();
-		int nt = n ;
-		int delta=(int)(b-a)/n;
+public class PrimeFinderThread extends Thread {
 
 
-		ArrayList<PrimeFinderThread> primeThread =new ArrayList<>();
-		ArrayList<Thread> threads =new ArrayList<>();
+    int a, b;
+    boolean bandera;
 
-		for(int i=0 ; i< n;i++){
-			PrimeFinderThread primet;
-			//System.out.println(1);
-			if(i!=n-1){
-				primet =new PrimeFinderThread(a+(i*delta),a+(i+delta)+delta);
-			}else{
-				primet=new PrimeFinderThread(a+(i*delta),b);
-			}
-			primeThread.add(primet);
-			primet.start();
-		}
-		 for(Thread th:threads){
-			 try {
-				 th.join();
-			 } catch (InterruptedException e) {
-				 e.printStackTrace();
-			 }
-		 }
+    double seconds;
 
-		 for(PrimeFinderThread pt:primeThread) {
-			 for (int i = 0; i < pt.getPrimes().size(); i++) {
-				 primes.add(pt.getPrimes().get(i));
-			 }
-		 }
-	}
+    private List<Integer> primes = new LinkedList<Integer>();
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				//System.out.println(2);
-				System.out.println(i);
-			}
-		}
-		
-		
-	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
-	}
+    public PrimeFinderThread(int a, int b) {
+        super();
+        this.a = a;
+        this.b = b;
+        bandera = true;
 
-	public void keyPressed(KeyEvent e){
+    }
 
-		int key =e.getKeyCode();
-		if(key==KeyEvent.VK_ENTER){
-			if(!currentThread().isAlive()){
+    public PrimeFinderThread(int a, int b, int n) {
+        super();
+        int nt = n;
+        int delta = (int) (b - a) / n;
+        bandera = true;
+        seconds = System.currentTimeMillis();
 
-			}
-		}
-	}
+        ArrayList<PrimeFinderThread> primeThread = new ArrayList<>();
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
-	
-	
-	
+        for (int i = 0; i < n; i++) {
+            PrimeFinderThread primeT;
+            //System.out.println(1);
+            if (i != n - 1) {
+                primeT = new PrimeFinderThread(a + (i * delta), a + (i + delta) + delta);
+            } else {
+                primeT = new PrimeFinderThread(a + (i * delta), b);
+            }
+            primeThread.add(primeT);
+            primeT.start();
+        }
+
+        while (true) {
+            if (System.currentTimeMillis() - seconds >= 5000) {
+                for (int i = 0; i < primeThread.size(); i++) {
+                    synchronized (this) {
+                        try {
+                            primeThread.get(i).wait();
+                            primeThread.get(i).changeBandera();
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+
+        for (PrimeFinderThread pt : primeThread) {
+            for (int i = 0; i < pt.getPrimes().size(); i++) {
+                primes.add(pt.getPrimes().get(i));
+            }
+        }
+        System.out.println(primes);
+    }
+
+    public void run() {
+        for (int i = a; i <= b; i++) {
+
+            if (isPrime(i)) {
+                primes.add(i);
+                //System.out.println(2);
+                //System.out.println(i);
+            }
+        }
+
+
+    }
+
+    boolean isPrime(int n) {
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public void changeBandera() {
+        this.bandera = !this.bandera;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+
 }
